7f23e2599d30b54325ceaae014eeed3a168b4260
Rajeel Siddiqui github <rajeelsiddiqui3@gmail.com>
2026-03-15 04:15:31 +0500

    push

--- routes/notification.route.js
+++ routes/notification.route.js
@@ -5,7 +5,7 @@ import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const route = express.Router();
 
-route.get("/", authMiddleware, getNotifications);
+route.get("/", getNotifications);
 route.post("/seen", authMiddleware, markAsSeen);
 route.post("/read-all", authMiddleware, markAllAsRead);
 
